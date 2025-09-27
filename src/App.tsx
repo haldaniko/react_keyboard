b65e205dbd532081ccde50e8cdf09aad333dd6a6
haldaniko <haldaniko@gmail.com>
2025-09-27 23:06:32 +0300

    fix lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
+import { Component } from 'react';
 
 interface AppState {
   pressedKey: string | null;
 }
 
 export class App extends Component<{}, AppState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      pressedKey: null,
-    };
-  }
+  state: AppState = {
+    pressedKey: null,
+  };
 
   handleKeyUp = (event: KeyboardEvent) => {
     this.setState({ pressedKey: event.key });
   };
 
   componentDidMount() {
-    document.addEventListener("keyup", this.handleKeyUp);
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("keyup", this.handleKeyUp);
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
 
   render() {
